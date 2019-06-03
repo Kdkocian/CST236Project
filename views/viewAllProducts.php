d96a62e7482e8548c4cf8517e8e721a9d100dc59
Kdkocian <krencent870@Gmail.com>
2019-06-02 23:03:58 -0500

    trying view all products file

--- views/viewAllProducts.php
+++ views/viewAllProducts.php
@@ -1,28 +1,20 @@
 <?php
-class displayAllProducts
-{
-public function _construct($products)
-    {
-            if(count((array)$products) > 0)
-                {
-                        echo '<link rel="stylesheet" type="text/css" href="style.css">';
-                        echo "<table>";
-                        echo "<tr><td>ID</td><td>First Name</td><td>Last Name</td></tr>";
-                        for ($x = 0; $x < count((array)$products); $x++)
-                        {
-                            echo "<tr>";
-                            echo "<td>" . $products[$x][0] . "</td>" .
-                                "<td>" . $products[$x][1] . "</td>" .
-                                "<td>" . $products[$x][2] . "</td>" ;
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                    }
-                    else
-                    {
-                        echo "No results were found";
-                }
-        }
-}
+require_once '../database.php';
+$db =  new myfuncs();
+$conn = $db->dbConnect();
 
+$sql = "SELECT * FROM products";
+
+$product = mysqli_real_escape_string($conn, $sql);
+echo "<table>";
+echo "<tr><td>Product ID </td><td>Product Name </td><td>Product Description</td></tr>";
+for($x = 0; $x < count((array)$product); $x++)
+{
+        echo "<tr>";
+        echo "<td>" . $product[$x][0] . "</td>" .
+            "<td>" . $product[$x][1] . "</td>" .
+            "<td>" . $product[$x][2] . "</td>" ;
+        echo "</tr>";
+}
+echo "</table>"
 ?>
