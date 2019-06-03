5351d148ff283915e3b01c256a106625122b87e8
Kdkocian <krencent870@Gmail.com>
2019-06-02 22:17:12 -0500

    trying to display in table will add images at later time... ON TIME CRUNCH

--- controllers/searchProduct.php
+++ controllers/searchProduct.php
@@ -11,6 +11,10 @@ $result = mysqli_query($conn, $sql);
 
 while($row = mysqli_fetch_array($result))
 {
+    echo '<link rel="stylesheet" type="text/css" href="style.css">';
+    echo "<table>";
+    echo "<tr><td>Product ID</td><td>Product Name</td><td>Product Description</td></tr>";
     echo $row['productID']," ", $row['productName'], " ", $row['productDescript'] ."<br>";
+    echo "</table>";
 }
 echo "<a href='../views/searchProductview.php'>Searchpage</a>";
--- views/viewAllProducts.php
+++ views/viewAllProducts.php
@@ -1,5 +1,28 @@
 <?php
-
-
+class displayAllProducts
+{
+public function _construct($products)
+    {
+            if(count((array)$products) > 0)
+                {
+                        echo '<link rel="stylesheet" type="text/css" href="style.css">';
+                        echo "<table>";
+                        echo "<tr><td>ID</td><td>First Name</td><td>Last Name</td></tr>";
+                        for ($x = 0; $x < count((array)$products); $x++)
+                        {
+                            echo "<tr>";
+                            echo "<td>" . $products[$x][0] . "</td>" .
+                                "<td>" . $products[$x][1] . "</td>" .
+                                "<td>" . $products[$x][2] . "</td>" ;
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
+                    else
+                    {
+                        echo "No results were found";
+                }
+        }
+}
 
 ?>
