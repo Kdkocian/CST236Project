95ea36e7c1b369d0bc2ba11920b9e98856c8903b
Kdkocian <krencent870@Gmail.com>
2019-06-02 22:19:14 -0500

    moving labels

--- controllers/searchProduct.php
+++ controllers/searchProduct.php
@@ -8,12 +8,11 @@ $searchresult = mysqli_real_escape_string($conn, $_GET['SearchBar']);
 $sql = "Select * from products where productName LIKE '%$searchresult%'";
 
 $result = mysqli_query($conn, $sql);
-
+echo "<tr><td>Product ID</td><td>Product Name</td><td>Product Description</td></tr>";
 while($row = mysqli_fetch_array($result))
 {
     echo '<link rel="stylesheet" type="text/css" href="style.css">';
     echo "<table>";
-    echo "<tr><td>Product ID</td><td>Product Name</td><td>Product Description</td></tr>";
     echo $row['productID']," ", $row['productName'], " ", $row['productDescript'] ."<br>";
     echo "</table>";
 }
