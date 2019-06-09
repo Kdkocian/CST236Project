c02a349ef2cf472f35825c35ac5c0ac82a14e047
Kdkocian <krencent870@Gmail.com>
2019-06-09 18:29:13 -0500

    changing view all products file

--- views/viewAllProducts.php
+++ views/viewAllProducts.php
@@ -11,21 +11,26 @@ $db= new funcs();
     </head>
     <body>
 <?php
+    echo '<ul id="content" class="list-unstyled">';
     $product = $db->getAllproducts();
     for($id = 0;$id < count($product);$id++){
+    echo '<li>';
         echo "<form action = '../controllers/manageProducts.php' method = 'POST'>";
-        echo "<div id='row'>";
-            echo "<div id=''>".$product[$id][0]."</div>";
-            echo "<div id=''>".$product[$id][1]."</div>";
-            echo "<div id=''>".$product[$id][2]."</div>";
-            echo "<div id=''>".$product[$id][3]."</div>";
-            echo '<div id=""><input type = "hidden" name = "ID" value = "'.$product[$id][0].'">';
-            echo '<input type="submit" name = "ADD" value="Add" /></div>';
-         echo '<div id=""><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "delete" value="Delete" />';
-        echo '<td><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "EDIT" value="Edit" /></div>';
-        echo "</form></tr>";
+            echo "<div class='row'>";
+                echo "<div class='col-md-2'>".$product[$id][0]."</div>";
+                echo "<div class='col-md-3'>".$product[$id][1]."</div>";
+                echo "<div class='col-md-3'>".$product[$id][2]."</div>";
+                echo "<div class='col-md-3'>".$product[$id][3]."</div>";
+                echo '<div classs="col-md-4"><input type = "hidden" name = "ID" value = "'.$product[$id][0].'">';
+                echo '<input type="submit" name = "ADD" value="Add" /></div>';
+                echo '<input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "delete" value="Delete" />';
+                echo '<td><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "EDIT" value="Edit" />';
+            echo "</div>";
+        echo "</form>";
+    echo '</li>';
     }
+    echo '</ul>';
     echo "<a href='../views/loginSuccessful.php'>Back to Login Success</a>";
-?>
+    ?>
     </body>
 </html>
