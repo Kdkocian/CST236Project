38588e151c2c5ec29f9b3c0636e0a035ff605627
Kdkocian <krencent870@Gmail.com>
2019-06-09 18:27:11 -0500

    changing view all products file

--- controllers/manageProducts.php
+++ controllers/manageProducts.php
@@ -5,7 +5,14 @@
     
     if(isset($_POST['EDIT']))
     {
+        $id = mysqli_real_escape_string($conn, $_POST['productID']);
+        $pname = mysqli_real_escape_string($conn, $_POST['productName']);
+        $descript = mysqli_real_escape_string($conn, $_POST['productDescript']);
         
+        $sql = "UPDATE products SET productName='$pname', productDescript='$descript' WHERE productID='$id'";
+        mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        header("Location: viewAllProducts.php");
     }
     
     if(isset($_POST['ADD']))
--- views/viewAllProducts.php
+++ views/viewAllProducts.php
@@ -7,36 +7,25 @@ $db= new funcs();
 <html>
     <head>
     	<title>Manage Users</title>
+    	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-    <table>
-    <tr>
-    	<th>Product ID</th>
-    	<th>Product Name</th>
-    	<th>Product Description</th>
-    	<th>Product in Stock</th>
-	</tr>
-    <?php
+<?php
     $product = $db->getAllproducts();
     for($id = 0;$id < count($product);$id++){
         echo "<form action = '../controllers/manageProducts.php' method = 'POST'>";
-        echo "<tr>";
-            echo "<td>".$product[$id][0]."</td>";
-            echo "<td>".$product[$id][1]."</td>";
-            echo "<td>".$product[$id][2]."</td>";
-            echo "<td>".$product[$id][3]."</td>";
-            echo '<td><input type = "hidden" name = "ID" value = "'.$product[$id][0].'">';
-            echo '<input type="submit" name = "ADD" value="Add" /></td>';
-        echo "</form>";
-        echo "<form action = '../controllers/manageProducts.php' method = 'POST'>";
-        echo '<td><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "delete" value="Delete" /></td>';
-        echo "</form></tr>";
-        echo "<form action = '../controllers/manageProducts.php' method = 'POST'>";
-        echo '<td><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "EDIT" value="Edit" /></td>';
+        echo "<div id='row'>";
+            echo "<div id=''>".$product[$id][0]."</div>";
+            echo "<div id=''>".$product[$id][1]."</div>";
+            echo "<div id=''>".$product[$id][2]."</div>";
+            echo "<div id=''>".$product[$id][3]."</div>";
+            echo '<div id=""><input type = "hidden" name = "ID" value = "'.$product[$id][0].'">';
+            echo '<input type="submit" name = "ADD" value="Add" /></div>';
+         echo '<div id=""><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "delete" value="Delete" />';
+        echo '<td><input type = "hidden" name = "ID" value = "'.$product[$id][0].'"><input type="submit" name = "EDIT" value="Edit" /></div>';
         echo "</form></tr>";
     }
     echo "<a href='../views/loginSuccessful.php'>Back to Login Success</a>";
-    ?>
-    </table>
+?>
     </body>
 </html>
