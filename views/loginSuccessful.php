01bf9be0e10860b0cf936d08192dca3dd3863eb4
Kdkocian <krencent870@Gmail.com>
2019-06-09 21:55:40 -0500

    seperated user and admin

--- views/loginSuccessful.php
+++ views/loginSuccessful.php
@@ -1,8 +1,7 @@
 <?php
 include 'functions.php';
-include_once 'database.php';
-$db= new funcs();
-
+require_once 'database.php';
+$db = new funcs();
 ?>
 <html>
 <head>
@@ -11,16 +10,17 @@ $db= new funcs();
 <h2>Login was successful</h2>
 <script type="text/javascript"> function logout(){ sessionStorage.clear(); window.location.href="login.html";}</script>
 <input type = "button" value = "Log Out" onclick = "logout();">
-<?php 
-if($db->getUseradmin())
+<?php
+    if($db->getUseradmin())
     {
-       echo "<a href='management.php'>View Users</a>";
-       echo "<a href='viewAllProducts.php'>View Products</a>";
-    }
+        echo "<a href='management.php'>View Users</a>";
+        echo "<a href='viewAllProducts.php'>ViewProducts</a>";
+    } 
     else 
     {
-       echo "<a href='viewAllProducts.php'>View Products</a>";
+        echo "<a href='viewAllProducts.php'>ViewProducts</a>";
     }
+
 ?>
 </body>
 </html>
