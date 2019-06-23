8dc934d4f83a128dbdd5a8fbec007be9c086ea24
Kdkocian <krencent870@Gmail.com>
2019-06-23 17:48:19 -0500

    added price to the products and the cart

--- views/checkout.php
+++ views/checkout.php
@@ -5,11 +5,11 @@ $conn = $db->dbConnect();
 
 echo "Your order is being processed";
 
-$sql = "DELETE FROM cart WHERE productIDs > 0";
+/*$sql = "DELETE FROM cart WHERE productIDs > 0";
 
 mysqli_query($conn, $sql);
 
-//mysqli_close($conn);
+mysqli_close($conn);*/
 
 echo "<a href='viewAllProducts.php'>Back to Store</a>";
 ?>
