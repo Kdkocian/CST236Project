f14fb542097ebfc43e097cd34c1eee68b5d28955
Kdkocian <krencent870@Gmail.com>
2019-07-01 13:09:53 -0500

    trying to pull from orderhistory

--- views/sRV.php
+++ views/sRV.php
@@ -7,15 +7,15 @@ $sql = "SELECT * FROM orderhistory";
 
 $result = mysqli_query($conn, $sql);
 echo "<tr>";
-echo "<th>ID</th>";
-echo "<th>UserID</th>";
+echo "<th>ID, </th>";
+echo "<th>UserID, </th>";
 echo "<th>Total Spent</th>";
 echo "</tr>";
 if ($result->num_rows > 0)
 {
     while($row = mysqli_fetch_array($result))
     {
-       echo $row['ID'].", ".$row['productNAme'].", ".$row['productPrice']."</br>";  
+       echo $row['ID'].", ".$row['productNAme'].", $".$row['productPrice']."</br>";  
     }
 }
 
